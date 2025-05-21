c099a0092ff998054b235c7f228fcb0b59d5b408
mestn1y <mestn1y@hotmail.com>
2025-05-21 20:23:29 +0300

    adding components ,install libs

--- crm/src/app/page.tsx
+++ crm/src/app/page.tsx
@@ -1,9 +1,12 @@
+export const dynamic = 'force-dynamic';
+import { headers } from 'next/headers';
 import StatusLabel, { Status } from './components/status-label';
 
-export default function Home() {
+export default async function Home() {
+  const now = new Date().toTimeString();
   return (
     <main>
-      <h1 className="text-xl">Home page</h1>
+      <h1 className="text-xl">Home page {now}</h1>
       <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
