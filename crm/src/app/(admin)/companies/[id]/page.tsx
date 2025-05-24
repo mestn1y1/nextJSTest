a7ca108b948de4a307a869c0b3b06b28a2a27097
mestn1y <mestn1y@hotmail.com>
2025-05-24 12:17:21 +0300

    adding sidebar,sidebar-items,layout component

--- crm/src/app/(admin)/companies/[id]/page.tsx
+++ crm/src/app/(admin)/companies/[id]/page.tsx
@@ -1,4 +1,4 @@
-import Header from '../../components/header';
+import Header from '../../../components/header';
 import React from 'react';
 
 export interface PageProps {
--- crm/src/app/(admin)/companies/page.tsx
+++ crm/src/app/(admin)/companies/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Header from '../components/header';
-import Toolbar from '../components/toolbar';
-import SearchInput from '../components/search-input';
-import AddCompanyButton from '../components/add-company-button';
-import CompanyTable from '../components/company-table';
-import CompanyRow from '../components/company-row';
-import { CompanyStatus } from '../../lib/api';
+import Header from '../../components/header';
+import Toolbar from '../../components/toolbar';
+import SearchInput from '../../components/search-input';
+import AddCompanyButton from '../../components/add-company-button';
+import CompanyTable from '../../components/company-table';
+import CompanyRow from '../../components/company-row';
+import { CompanyStatus } from '../../../lib/api';
 export interface PageProps {}
 
 export default function Page({}: PageProps) {
